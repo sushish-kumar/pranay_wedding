d696e80eb7e4118f4f6f05e85e471e15bfb81b60
sushish-kumar <sushish.kumar.cse14@iitbhu.ac.in>
2026-02-22 14:08:44 +0530

    Update main.js
    Update timer target time.

--- public/js/main.js
+++ public/js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("7 March 2026 9:56:00 GMT+01:00");			
+		var endTime = new Date("11 March 2026 11:59:00 GMT+05:30");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
